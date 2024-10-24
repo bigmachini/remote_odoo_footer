342d3a067460894afc63f3f7d2a778b1fd2009a3
bigmachini <elisha.wambiji@bigmachini.net>
2024-10-24 15:10:17 +0000

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Remove Odoo Footer",
+
+    'summary': "This code is used to remove the odoo footer from the website.",
+
+    'description': """
+        Tired of the powered by Odoo footer? This code is used to remove the odoo footer from the website.
+    """,
+
+    'author': "Bigmachini Enterprises LTD",
+    'website': "https://bigmachini.net",
+    'category': 'Website',
+    'version': '1.0',
+    'depends': ['website'],
+    'data': [
+        'views/website_templates.xml',
+    ],
+}
